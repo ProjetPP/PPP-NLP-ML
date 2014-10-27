ac032ba8568dbb5816f901fb41a6dd735dd67d27
Raphaël <raphael.charrondiere@ens-lyon.fr>
2014-10-27 18:51:35 +0100

    Implementation of (un)compact in request trees

--- reformulation/reformulation/Transformator.cpp
+++ reformulation/reformulation/Transformator.cpp
@@ -1,1 +1,96 @@
 #include "Transformator.h"
+#include <exception>
+
+SimplifiedRequestTree::SimplifiedRequestTree(SimplifiedRequestTree::Type type,SimplifiedRequestTree* subjectReq, SimplifiedRequestTree* objectReq):
+mytype(type),subject(subjectReq),object(objectReq)
+{
+  if(type!=Type::PREDICATE)
+    throw std::exception();//bad constructor
+}
+
+SimplifiedRequestTree::SimplifiedRequestTree(SimplifiedRequestTree::Type type)
+{
+  mytype=type;
+  if(type==Type::PREDICATE)
+    throw std::exception();//bad constructor
+  
+}
+
+SimplifiedRequestTree::~SimplifiedRequestTree()
+{
+  if(mytype==Type::PREDICATE)
+  {
+    delete subject;
+    delete object;
+  }
+}
+
+
+RequestTree::RequestTree(RequestTree::Type type, Request data):
+mytype(type),data(data)
+{
+  if(type==Type::PREDICATE)
+    throw std::exception();//bad constructor
+  
+}
+
+RequestTree::RequestTree(RequestTree::Type type, Request predicate, RequestTree* subjectReq, RequestTree* objectReq):
+mytype(type),data(predicate),subject(subjectReq),object(objectReq)
+{
+  if(type!=Type::PREDICATE)
+    throw std::exception();//bad constructor
+}
+
+RequestTree::~RequestTree()
+{
+  if(this->mytype==Type::PREDICATE)
+  {
+    delete subject;
+    delete object;
+  }
+}
+
+RequestTree::RequestTree(word data,Functions* func,Dictionary* dico,float delta)
+{
+  mytype=Type::PREDICATE;
+  Request req=func->uncompact(data);
+  this->data=(*dico)[dico->findnearestPredicate(req)];
+  Request subjectFound=(*dico)[dico->findnearestSubject(req)];
+  Request objectFound=(*dico)[dico->findnearestObject(req)];
+  
+  if(subjectFound.getDistance2subject(req)>delta)
+    subject=new RequestTree(req.getSubject(),func,dico,delta);
+  else
+    subject=new RequestTree(Type::SUBJECT,subjectFound);
+  
+  if(objectFound.getDistance2object(req)>delta)
+    object=new RequestTree(req.getObject(),func,dico,delta);
+  else
+    object=new RequestTree(Type::SUBJECT,objectFound);
+    
+}
+
+word RequestTree::compact(Functions* func)
+{
+  if(mytype==Type::SUBJECT)return data.getSubject();
+  if(mytype==Type::OBJECT)return data.getObject();
+  return func->compact(Request(this->subject->compact(func),this->data.getPredicate(),this->object->compact(func)));
+}
+
+SimplifiedRequestTree* RequestTree::getSimplifiedRequestTree()
+{
+  if(mytype==Type::OBJECT)return new SimplifiedRequestTree(SimplifiedRequestTree::OBJECT);
+  if(mytype==Type::SUBJECT)return new SimplifiedRequestTree(SimplifiedRequestTree::SUBJECT);
+  return new SimplifiedRequestTree(SimplifiedRequestTree::PREDICATE,subject->getSimplifiedRequestTree(),object->getSimplifiedRequestTree());
+}
+
+
+Transformator::Transformator(Functions* functions, Dictionary* dictionary,float precision):
+func(functions),dico(dictionary),delta(precision)
+{
+}
+
+void Transformator::reformulation(RequestTree rt)
+{
+  
+}
--- reformulation/reformulation/Transformator.h
+++ reformulation/reformulation/Transformator.h
@@ -1,5 +1,53 @@
 #ifndef TRANSFORMATOR_H
 #define TRANSFORMATOR_H
+#include "Request.h"
+#include "Dictionary.h"
+#include "Functions.h"
+#include <limits>
 
+class SimplifiedRequestTree
+{
+  public:
+  enum Type{SUBJECT,OBJECT,PREDICATE};
+  SimplifiedRequestTree(SimplifiedRequestTree::Type type);
+  SimplifiedRequestTree(SimplifiedRequestTree::Type type,SimplifiedRequestTree* subjectReq,SimplifiedRequestTree* objectReq);
+  ~SimplifiedRequestTree();
+  
+private:
+  Type mytype;
+  SimplifiedRequestTree* subject;
+  SimplifiedRequestTree* object;
+};
+
+class RequestTree
+{
+public:
+  enum Type{SUBJECT,OBJECT,PREDICATE};
+  RequestTree(RequestTree::Type, Request data);
+  RequestTree(RequestTree::Type, Request predicate,RequestTree* subjectReq,RequestTree * objectReq);
+  RequestTree(word data,Functions* func,Dictionary* dico,float delta);
+  ~RequestTree();
+  
+  word compact(Functions* func);
+  SimplifiedRequestTree* getSimplifiedRequestTree();
+private:
+  Type mytype;
+  Request data;
+  RequestTree* subject;
+  RequestTree* object;
+};
+
+class Transformator
+{
+public:
+  Transformator(Functions* functions, Dictionary* dictionary, float precision=numeric_limits<float>::infinity());
+  
+  void reformulation(RequestTree rt);
+  
+private:
+  Functions* func;
+  Dictionary* dico;
+  float delta;
+};
 
 #endif
